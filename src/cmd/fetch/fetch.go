76edda87ff629d41b5d7d2919c37ebda941d3f59
Vincent Dansereau <dansereauvincent@gmail.com>
2023-05-06 13:45:49 -0400

    merge remote

---
+++ src/cmd/fetch/fetch.go
@@ -0,0 +1,79 @@
+package fetch
+
+import (
+	"strings"
+
+	"github.com/13013SwagR/semver-manager/smgr/internal/utils"
+	smvr "github.com/13013SwagR/semver-manager/smgr/pkg/semver"
+	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
+)
+
+type config struct {
+	Token      string `san:"trim"`
+	Repository string `san:"trim"`
+	Owner      string `san:"trim"`
+	Platform   string `san:"trim"`
+	dryRun     bool
+	Filters    smvr.Filters
+}
+
+func NewFetchCommand() *cobra.Command {
+	config := &config{}
+	var fetchCmd = &cobra.Command{
+		Use:   "fetch",
+		Short: "Fetch semver tags from a registry or repository.",
+		Long: `Fetch semver tags from a registry or repository and
+    sorted by highest version first. Includes tags starting 'v' as in 'v0.0.0`,
+
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return utils.InitializeConfig(cmd)
+		},
+
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := utils.SanitizeInputs(config); err != nil {
+				klog.Errorf("CLI argument error: %w", err)
+				panic(config)
+			}
+
+			dryRun, _ := cmd.PersistentFlags().GetBool("dry-run")
+			config.dryRun = dryRun
+
+			return RunFetchSemverTags(config, cmd)
+		},
+	}
+	fetchCmd.Flags().StringVarP(&config.Owner, "owner", "o", "", "The owner of the registry or repository")
+	fetchCmd.Flags().StringVarP(&config.Repository, "repo", "r", "", "The repository or registry to fetch the Semver tags from")
+	fetchCmd.Flags().StringVarP(&config.Token, "token", "t", "", "The token to access the repository")
+	fetchCmd.Flags().StringVarP(&config.Platform, "platform", "p", "github", "The platform to fetch the Semver from, options: github")
+	fetchCmd.Flags().BoolVarP(&config.Filters.Highest, "highest", "H", false, "Fetches only the highest Semver tag")
+	fetchCmd.Flags().BoolVarP(&config.Filters.Release, "release", "R", false, "Fetches only Release Semver tag (x.x.x)")
+
+	return fetchCmd
+}
+
+func RunFetchSemverTags(config *config, cmd *cobra.Command) error {
+
+	semverSvc := newSemverSvc(config.dryRun, config.Platform, config.Token)
+
+	klog.V(1).Info("Fetching tags...")
+	semverTags, err := semverSvc.FetchSemverTags(config.Owner, config.Repository, &config.Filters)
+	if err != nil {
+		return err
+	}
+
+	cmd.Println(strings.Join(semverTags, " "))
+	return nil
+}
+
+func newSemverSvc(dryRun bool, platform, token string) smvr.Semver {
+	if len(platform) == 0 {
+		platform = "github"
+	}
+	if dryRun {
+		platform = "dry-run"
+	}
+	semverSvc := smvr.NewSemverSvc(platform, token)
+
+	return semverSvc
+}
---
+++ src/cmd/smgr/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	fetch "github.com/13013SwagR/semver-manager/smgr/cmd/fetch"
+	"github.com/13013SwagR/semver-manager/smgr/internal/utils"
+	"k8s.io/klog/v2"
+)
+
+type fetchFlags struct {
+	Token      string `san:"trim"`
+	Repository string `san:"trim"`
+	Owner      string `san:"trim"`
+	Platform   string `san:"trim"`
+}
+
+type config struct {
+	dryRun bool
+}
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Set("logtostderr", "false")
+	flag.Set("alsologtostderr", "false")
+	flag.Parse()
+
+	cmd := NewRootCommand(os.Stdout)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func NewRootCommand(output io.Writer) *cobra.Command {
+	klog.SetOutput(output)
+	config := config{
+		dryRun: false,
+	}
+
+	rootCmd := &cobra.Command{
+		Use:   "smgr",
+		Short: "Manage Semantic Versioning compliant versions.",
+		Long:  `Manage Semantic Versioning compliant versions and integrate with popular or registry platform to facilitate the task.`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return utils.InitializeConfig(cmd)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+
+	rootCmd.PersistentFlags().BoolVar(&config.dryRun, "dry-run", false, "Execute the command in dry-run mode")
+	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+	rootCmd.AddCommand(fetch.NewFetchCommand())
+
+	return rootCmd
+}
