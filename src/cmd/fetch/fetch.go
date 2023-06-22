9f511dd55cdb2edacbf32b2ade372728f78aa81f
Vincent Dansereau <32471663+13013SwagR@users.noreply.github.com>
2023-06-21 22:19:11 -0400

    Update fetch.go

--- src/cmd/fetch/fetch.go
+++ src/cmd/fetch/fetch.go
@@ -10,53 +10,6 @@ import (
 	"k8s.io/klog"
 )
 
-// fetchCmd represents the fetch command
-// var fetchCmd = &cobra.Command{
-// 	Use:   "fetch",
-// 	Short: "Fetch is a CLI tool for fetching versions",
-// 	Long:  `Fetch is a CLI tool for fetching versions from some source.`,
-// 	RunE: func(cmd *cobra.Command, args []string) error {
-// 		fmt.Println("Fetching versions...")
-// 		fetcher, err := fetch.NewFetcher(&util.DatasourceConfig{})
-// 		if err != nil {
-// 			return err
-// 		}
-// 		versions, err := fetcher.FetchTags()
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		filters := []filter.FilterFunc{}
-
-// if cmd.Flags().Changed("major") {
-// 	filters = append(filters, filter.MajorVersionStream(major))
-// }
-// if cmd.Flags().Changed("minor") {
-// 	filters = append(filters, filter.MinorVersionStream(major, minor))
-// }
-// if cmd.Flags().Changed("patch") {
-// 	filters = append(filters, filter.PatchVersionStream(major, minor, patch))
-// }
-// if cmd.Flags().Changed("prerelease") {
-// 	filters = append(filters, filter.PreReleaseVersionStream(models.Release{}, models.PRVersion{}))
-// }
-// 		if highest {
-// 			filters = append(filters, filter.Highest())
-// 		}
-
-// 		filteredVersions, err := filter.ApplyFilters(versions, filters...)
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		cmd.OutOrStdout().Write([]byte(filteredVersions.String()))
-// 		return nil
-// 	},
-// }
-
-// func init() {
-// 	fetchCmd.Flags().BoolVar(&highest, "highest", false, "Select the highest versions")
-// }
-
 type config struct {
 	Token      string `san:"trim"`
 	Repository string `san:"trim"`
