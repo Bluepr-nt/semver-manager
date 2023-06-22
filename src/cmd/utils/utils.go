8bc545cee0c7ef8b80b62aeb9a699fbf37675924
Vincent Dansereau <32471663+13013SwagR@users.noreply.github.com>
2023-06-21 22:24:15 -0400

    Update utils.go

--- src/cmd/utils/utils.go
+++ src/cmd/utils/utils.go
@@ -16,40 +16,26 @@ const (
 
 func InitializeConfig(cmd *cobra.Command) error {
 	v := viper.New()
-
-	// Set the base name of the config file, without the file extension.
 	v.SetConfigName(defaultConfigFilename)
-
 	v.AddConfigPath(".")
-
 	if err := v.ReadInConfig(); err != nil {
-		// It's okay if there isn't a config file
 		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
 			return err
 		}
 	}
-
 	v.SetEnvPrefix(envPrefix)
-
 	v.AutomaticEnv()
-
-	// Bind the current command's flags to viper
-	// BindFlags(cmd, v)
-	// v.BindPFlags(cmd.Flags())
-
+	
 	return nil
 }
 
 func BindFlags(cmd *cobra.Command, v *viper.Viper) {
 	cmd.Flags().VisitAll(func(f *pflag.Flag) {
-		// Environment variables can't have dashes in them, so bind them to their equivalent
-		// keys with underscores, e.g. --favorite-color to STING_FAVORITE_COLOR
 		if strings.Contains(f.Name, "-") {
 			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
 			v.BindEnv(f.Name, fmt.Sprintf("%s_%s", envPrefix, envVarSuffix))
 		}
 
-		// Apply the viper config value to the flag when the flag is not set and viper has a value
 		if !f.Changed && v.IsSet(f.Name) {
 			val := v.Get(f.Name)
 			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
