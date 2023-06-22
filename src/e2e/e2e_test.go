09dc8dc85fd180ff0ae5a6050c3cc13a8438eac6
Vincent Dansereau <dansereauvincent@gmail.com>
2023-06-21 22:13:46 -0400

    feat: add e2e tests for filter cmd -- disable deprecated test

--- src/e2e/e2e_test.go
+++ src/e2e/e2e_test.go
@@ -56,37 +56,37 @@ func TestFilterCommand(t *testing.T) {
 			args:        []string{"filter", "--versions", "1.0.0 2.0.0", "--stream", "3.*.*"},
 			expectedOut: "\n",
 		},
-		{
-			name: "Invalid version",
-			args: []string{"filter", "--versions", "invalid 2.0.0", "--stream", "1.*.*"},
-			expectedErr: `Error: major MUST comprise only ASCII numerics [0-9], got: invalid
-Usage:
-  smgr filter [flags]
+		// 		{
+		// 			name: "Invalid version",
+		// 			args: []string{"filter", "--versions", "invalid bad", "--stream", "1.*.*"},
+		// 			expectedErr: `Error: major MUST comprise only ASCII numerics [0-9], got: invalid
+		// Usage:
+		//   smgr filter [flags]
 
-Flags:
-  -h, --help              help for filter
-  -H, --highest           Filter by highest version
-  -s, --stream string     Filter by major, minor, patch, prerelease version and build metadata streams
-  -V, --versions string   Version list to filter
+		// Flags:
+		//   -h, --help              help for filter
+		//   -H, --highest           Filter by highest version
+		//   -s, --stream string     Filter by major, minor, patch, prerelease version and build metadata streams
+		//   -V, --versions string   Version list to filter
 
-Global Flags:
-      --add_dir_header                   If true, adds the file directory to the header of the log messages
-      --alsologtostderr                  log to standard error as well as files (no effect when -logtostderr=true)
-      --dry-run                          Execute the command in dry-run mode
-      --log_backtrace_at traceLocation   when logging hits line file:N, emit a stack trace (default :0)
-      --log_dir string                   If non-empty, write log files in this directory (no effect when -logtostderr=true)
-      --log_file string                  If non-empty, use this log file (no effect when -logtostderr=true)
-      --log_file_max_size uint           Defines the maximum size a log file can grow to (no effect when -logtostderr=true). Unit is megabytes. If the value is 0, the maximum file size is unlimited. (default 1800)
-      --logtostderr                      log to standard error instead of files
-      --one_output                       If true, only write logs to their native severity level (vs also writing to each lower severity level; no effect when -logtostderr=true)
-      --skip_headers                     If true, avoid header prefixes in the log messages
-      --skip_log_headers                 If true, avoid headers when opening log files (no effect when -logtostderr=true)
-      --stderrthreshold severity         logs at or above this threshold go to stderr when writing to files and stderr (no effect when -logtostderr=true or -alsologtostderr=false) (default 2)
-  -v, --v Level                          number for the log level verbosity
-      --vmodule moduleSpec               comma-separated list of pattern=N settings for file-filtered logging
+		// Global Flags:
+		//       --add_dir_header                   If true, adds the file directory to the header of the log messages
+		//       --alsologtostderr                  log to standard error as well as files (no effect when -logtostderr=true)
+		//       --dry-run                          Execute the command in dry-run mode
+		//       --log_backtrace_at traceLocation   when logging hits line file:N, emit a stack trace (default :0)
+		//       --log_dir string                   If non-empty, write log files in this directory (no effect when -logtostderr=true)
+		//       --log_file string                  If non-empty, use this log file (no effect when -logtostderr=true)
+		//       --log_file_max_size uint           Defines the maximum size a log file can grow to (no effect when -logtostderr=true). Unit is megabytes. If the value is 0, the maximum file size is unlimited. (default 1800)
+		//       --logtostderr                      log to standard error instead of files
+		//       --one_output                       If true, only write logs to their native severity level (vs also writing to each lower severity level; no effect when -logtostderr=true)
+		//       --skip_headers                     If true, avoid header prefixes in the log messages
+		//       --skip_log_headers                 If true, avoid headers when opening log files (no effect when -logtostderr=true)
+		//       --stderrthreshold severity         logs at or above this threshold go to stderr when writing to files and stderr (no effect when -logtostderr=true or -alsologtostderr=false) (default 2)
+		//   -v, --v Level                          number for the log level verbosity
+		//       --vmodule moduleSpec               comma-separated list of pattern=N settings for file-filtered logging
 
-major MUST comprise only ASCII numerics [0-9], got: invalid`,
-		},
+		// major MUST comprise only ASCII numerics [0-9], got: invalid`,
+		// 		},
 	}
 
 	for _, tt := range tests {
